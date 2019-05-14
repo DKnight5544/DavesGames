9413fba0c8d0c10266c6b7c3b3950680a43b0cae
DKnight5544 <dave@mostlyspam.com>
2019-05-14 09:36:41 -0700

    Add project files.

---
+++ DavesGames/Games/TheChase/TheChase.js
@@ -0,0 +1,30 @@
+﻿
+let _InnerDiv;
+
+
+function body_onload() {
+
+    _InnerDiv = document.getElementById("InnerDiv");
+
+}
+
+
+function buildTable() {
+    
+    let table = document.createElement('table');
+    let tr;
+    let td;
+
+    _InnerDiv.innerHTML = table;
+
+    tr = document.createElement('tr');
+
+    td = document.createElement('td');
+
+    tr.appendChild(td);
+   
+    table.appendChild(tr);
+    
+
+
+}
